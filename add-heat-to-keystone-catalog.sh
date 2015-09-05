f6ceddd944e0ddb58847fa84fefa3f981ca8e593
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2015-09-05 22:59:14 +0100

    minor fixes

--- add-heat-to-keystone-catalog.sh
+++ add-heat-to-keystone-catalog.sh
@@ -21,35 +21,36 @@ then
         --description "Orchestration"
       keystone endpoint-create \
         --service-id $(keystone service-list | awk '/ orchestration / {print $2}') \
-        --publicurl http://${PUBLIC_IP}:8004/v1/%\(tenant_id\)s \
-        --internalurl http://${PUBLIC_IP}:8004/v1/%\(tenant_id\)s \
-        --adminurl http://${PUBLIC_IP}:8004/v1/%\(tenant_id\)s \
+        --publicurl http://${FLOATING_IP}:8004/v1/%\(tenant_id\)s \
+        --internalurl http://${FLOATING_IP}:8004/v1/%\(tenant_id\)s \
+        --adminurl http://${FLOATING_IP}:8004/v1/%\(tenant_id\)s \
         --region ${REGION}
       keystone endpoint-create \
         --service-id $(keystone service-list | awk '/ cloudformation / {print $2}') \
-        --publicurl http://${PUBLIC_IP}:8000/v1 \
-        --internalurl http://${PUBLIC_IP}:8000/v1 \
-        --adminurl http://${PUBLIC_IP}:8000/v1 \
+        --publicurl http://${FLOATING_IP}:8000/v1 \
+        --internalurl http://${FLOATING_IP}:8000/v1 \
+        --adminurl http://${FLOATING_IP}:8000/v1 \
         --region ${REGION}
     fi
 else
-  echo " * Keystone service catalog will not be updated at this point"
-  echo "You can update it later by changing networkrc file or "
-  echo "you can update manually by enterring:"
-  echo "1. keystone service-create --name heat --type orchestration \
+  echo ' * Keystone service catalog will not be updated at this point'
+  echo 'You can update it later by changing networkrc file or '
+  echo 'you can update manually by enterring:'
+  echo "1. keystone service-create --name heat --type orchestration \\
         --description 'Orchestration'"
-  echo "2. keystone service-create --name heat-cfn --type cloudformation \
+  echo "2. keystone service-create --name heat-cfn --type cloudformation \\
         --description 'Orchestration'"
-  echo "3. keystone endpoint-create \
-        --service-id $(keystone service-list | awk '/ orchestration / {print $2}') \
-        --publicurl http://${PUBLIC_IP}:8004/v1/%\(tenant_id\)s \
-        --internalurl http://${PUBLIC_IP}:8004/v1/%\(tenant_id\)s \
-        --adminurl http://${PUBLIC_IP}:8004/v1/%\(tenant_id\)s \
+  echo "3. keystone endpoint-create \\
+        --service-id \$\(keystone service-list | awk '/ orchestration / {print $2}') \\
+        --publicurl http://${FLOATING_IP}:8004/v1/%\(tenant_id\)s \\
+        --internalurl http://${FLOATING_IP}:8004/v1/%\(tenant_id\)s \\
+        --adminurl http://${FLOATING_IP}:8004/v1/%\(tenant_id\)s \\
         --region ${REGION}"
-  echo "4. keystone endpoint-create \
-        --service-id $(keystone service-list | awk '/ cloudformation / {print $2}') \
-        --publicurl http://${PUBLIC_IP}:8000/v1 \
-        --internalurl http://${PUBLIC_IP}:8000/v1 \
-        --adminurl http://${PUBLIC_IP}:8000/v1 \
+  echo "4. keystone endpoint-create \\
+        --service-id \$\(keystone service-list | awk '/ cloudformation / {print $2}') \\
+        --publicurl http://${FLOATING_IP}:8000/v1 \\
+        --internalurl http://${FLOATING_IP}:8000/v1 \\
+        --adminurl http://${FLOATING_IP}:8000/v1 \\
         --region ${REGION}"
+
 fi
